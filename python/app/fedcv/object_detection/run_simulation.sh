38a1fa21518bfda4060e589eb7b2d74a4a374118
czkyx <408258313@qq.com>
2022-08-26 11:45:46 +0800

    Update run_simulation.sh
    my environment is google colab, need to fix three bugs:
    1.  mpi have to run  --allow-run-as-root 
    2. python3 otherwise no module fedml...
    3. "ModuleNotFoundError: No module named 'fedml.simulation.mpi.async_fedavg'

--- python/app/fedcv/object_detection/run_simulation.sh
+++ python/app/fedcv/object_detection/run_simulation.sh
@@ -3,5 +3,8 @@ WORKER_NUM=$1
 PROCESS_NUM=`expr $WORKER_NUM + 1`
 echo $PROCESS_NUM
 hostname > mpi_host_file
-$(which mpirun) -np $PROCESS_NUM \
-python main_fedml_object_detection.py --cf config/simulation/fedml_config.yaml
+$(which mpirun) -np $PROCESS_NUM  --allow-run-as-root \
+
+##  SOLVE the bug: "ModuleNotFoundError: No module named 'fedml.simulation.mpi.async_fedavg' "  my environment is google colab
+!cp -r /content/drive/MyDrive/FedML/python/fedml/simulation/mpi/async_fedavg  /usr/local/lib/python3.7/dist-packages/fedml/simulation/mpi/
+python3 main_fedml_object_detection.py --cf config/simulation/fedml_config.yaml
